1d9174a2742349c72ac9c8c0fa8bbce7ee71428e
vivaldi <pocari@ubuntu.(none)>
2013-01-14 05:38:52 -0800

    mod setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ DOT_FILES=(.zshrc .zshrc.linux .zshrc.osx .emacs.d .tmux.conf)
 for file in ${DOT_FILES[@]}
 do
     src_file=$(echo ${file} | sed 's/^\./_/')
-    ln -s $HOME/dotfiles/$src_file $HOME/$file
+    if [ ! -L $HOME/$file -a -f $HOME/$file ]; then
+      mv $HOME/$file $HOME/$file_
+    fi
+
+    ln -snf $HOME/dotfiles/$src_file $HOME/$file
 done
 
