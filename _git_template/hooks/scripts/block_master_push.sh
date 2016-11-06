aabb8d623179c92ff0f0863d6d426b66b13c35e8
pocari <caffelattenonsugar@gmail.com>
2016-11-06 17:19:11 +0900

    masterにpush出来るオプションを追加

--- _git_template/hooks/scripts/block_master_push.sh
+++ _git_template/hooks/scripts/block_master_push.sh
@@ -2,8 +2,8 @@
 
 while read local_ref local_sha1 remote_ref remote_sha1
 do
-  if [[ "${remote_ref##refs/heads/}" = "master" ]]; then
-    echo "*ERROR*: Do not push to master branch!!!"
+  if [[ "$FORCE_PUT_MASTER" != "1" && "${remote_ref##refs/heads/}" = "master" ]]; then
+    echo "*ERROR*: Do not push to master branch!!! or execute with FORCE_PUT_MASTER=1"
     exit 1
   fi
 done
