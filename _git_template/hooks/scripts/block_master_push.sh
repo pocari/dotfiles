9abada9d0931abdb28e6b575ab091e31cea8f5f8
pocari <caffelattenonsugar@gmail.com>
2016-11-25 18:31:31 +0900

    masterへのプッシュガードスクリプトに色を付けた

--- _git_template/hooks/scripts/block_master_push.sh
+++ _git_template/hooks/scripts/block_master_push.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+RED='\033[0;31m'
+NC='\033[0m'
 while read local_ref local_sha1 remote_ref remote_sha1
 do
   if [[ "$FORCE_PUT_MASTER" != "1" && "${remote_ref##refs/heads/}" = "master" ]]; then
-    echo "*ERROR*: Do not push to master branch!!! or execute with FORCE_PUT_MASTER=1"
+    echo -e "${RED}*ERROR*${NC}: Do not push to master branch!!! or execute with '${RED}FORCE_PUT_MASTER=1${NC} git push ...'"
     exit 1
   fi
 done
