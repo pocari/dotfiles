61c3a4b116e82642f3e49a8daba3642177264e9e
pocari <caffelattenonsugar@gmail.com>
2017-03-26 00:39:24 +0900

    ちょっと変更

--- setup.sh
+++ setup.sh
@@ -3,16 +3,33 @@
 # TODO vimとneovimを共存させてるので
 # ~/.config/nvim を ~/.vimディレクトリへのsymlinkにする必要がある
 # ~/.config自体は他の処理からも作成されるので、まだ無い場合のみ作成する
-DOT_FILES=(.zshrc .zshenv .zprofile .tmux.conf .gvimrc .vimrc .vim .zsh .tmuxinator .gitconfig .globalrc .peco .git_template .ctags .pryrc .tigrc)
+DOT_FILES=(
+  .zshrc
+  .zshenv
+  .zprofile
+  .tmux.conf
+  .gvimrc
+  .vimrc
+  .vim
+  .zsh
+  .tmuxinator
+  .gitconfig
+  .globalrc
+  .peco
+  .git_template
+  .ctags
+  .pryrc
+  .tigrc
+)
 
 for file in ${DOT_FILES[@]}
 do
-    src_file=$(echo ${file} | sed 's/^\./_/')
-    if [ ! -L $HOME/$file -a -f $HOME/$file ]; then
-      echo "WARN: found no symlink file: $HOME/$file. rename to $HOME/${file}_"
-      mv $HOME/$file $HOME/${file}_
-    fi
+  src_file=$(echo ${file} | sed 's/^\./_/')
+  if [ ! -L $HOME/$file -a -f $HOME/$file ]; then
+    echo "WARN: found no symlink file: $HOME/$file. rename to $HOME/${file}_"
+    mv $HOME/$file $HOME/${file}_
+  fi
 
-    ln -snf $HOME/dotfiles/$src_file $HOME/$file
+  ln -snf $HOME/dotfiles/$src_file $HOME/$file
 done
 
